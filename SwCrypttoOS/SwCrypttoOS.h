cd52e1a2c8470c625f104d4f13ae5b9e697f12e6
Ivan Rep <ivan.rep@undabot.com>
2016-09-09 13:56:18 +0200

    Added tvOS target

---
+++ SwCrypttoOS/SwCrypttoOS.h
@@ -0,0 +1,19 @@
+//
+//  SwCrypttoOS.h
+//  SwCrypttoOS
+//
+//  Created by Undabot Rep on 09/09/16.
+//  Copyright © 2016 irl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwCrypttoOS.
+FOUNDATION_EXPORT double SwCrypttoOSVersionNumber;
+
+//! Project version string for SwCrypttoOS.
+FOUNDATION_EXPORT const unsigned char SwCrypttoOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwCrypttoOS/PublicHeader.h>
+
+
